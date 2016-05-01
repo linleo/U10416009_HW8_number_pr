ac7fc336bf2dc8871694f4578c72341040d25472
linleo <u10416009@go.utaipei.edu.tw>
2016-05-01 11:23:51 +0800

    Update NumberAccuracy.java
    Add comments.

--- NumberAccuracy.java
+++ NumberAccuracy.java
@@ -2,12 +2,16 @@ import java.math.*;
 
 public class NumberAccuracy
 {
+	//constructor
 	NumberAccuracy(){}	
 		
-	BigDecimal operation(double input1, double input2, int scale)
+	BigDecimal operation(String input1, String input2, int scale)
 	{
+		//first number
 		BigDecimal num1 = new BigDecimal(input1);
+		//second number
 		BigDecimal num2 = new BigDecimal(input2); 
+		//return first number divide second number
 		return num1.divide(num2, scale, BigDecimal.ROUND_HALF_UP);
 	}
 }
