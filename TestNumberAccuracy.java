17224003ed0fafc98ea0172de13f8ea04ef24e44
linleo <u10416009@go.utaipei.edu.tw>
2016-05-02 16:21:04 +0800

    Update TestNumberAccuracy.java
    User input first number, second number, and operator then do operation.

--- TestNumberAccuracy.java
+++ TestNumberAccuracy.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.math.*;
 
 public class TestNumberAccuracy
 {
@@ -8,34 +7,54 @@ public class TestNumberAccuracy
 		NumberAccuracy accuracy = new NumberAccuracy();
 		
 		Scanner input = new Scanner(System.in);
-		//user input the dividend
-		System.out.print("Enter the dividend : ");
+		//user input the first number
+		System.out.print("Enter the first number : ");
 		String num1 = input.next();
-		//user input the divisor
-		System.out.print("Enter the divisor : ");
+		//user input the second number
+		System.out.print("Enter the second number : ");
 		String num2 = input.next();
-		//user input the accuracy
-		System.out.print("Enter the accuracy you want : ");
-		int scale = input.nextInt();
-		BigDecimal ans = accuracy.operation(num1, num2, scale);
-		//display result
-		System.out.println("The answer is " + ans);
+		System.out.print("Enter the operator to do operation (ex:+,-,*,/) : ");
+		//user input operator
+		String operator = input.next();
+		if (operator.equals("/"))
+		{
+			//user input the accuracy
+			System.out.print("Enter the accuracy you want : ");
+			int scale = input.nextInt();
+			//display result
+			System.out.println("The answer is " + accuracy.operation(num1, num2, scale));
+		}
+		else 
+		{
+			System.out.println("The answer is " + accuracy.operation(num1, num2, operator));
+		}
+		
 		//ask user again or not
 		System.out.print("Again?(Y/N)\n");
 		String again = input.next();
 		while (again.equals("Y") || again.equals("y") || again.equals("Yes") || again.equals("yes"))
 		{
-			//user input the dividend
-			System.out.print("Enter the dividend : ");
+			//user input the second number
+			System.out.print("Enter the first number : ");
 			num1 = input.next();
-			//user input the divisor
-			System.out.print("Enter the divisor : ");
+			//user input the second number
+			System.out.print("Enter the second number : ");
 			num2 = input.next();
-			//user input the accuracy
-			System.out.print("Enter the accuracy you want : ");
-			scale = input.nextInt();
-			ans = accuracy.operation(num1, num2, scale);
-			System.out.println("The answer is " + ans);
+			System.out.print("Enter the operator to do operation (ex:+,-,*,/) : ");
+			//user input operator
+			operator = input.next();
+			if (operator.equals("/"))
+			{
+				//user input the accuracy
+				System.out.print("Enter the accuracy you want : ");
+				int scale = input.nextInt();
+				//display result
+				System.out.println("The answer is " + accuracy.operation(num1, num2, scale));
+			}
+			else 
+			{
+				System.out.println("The answer is " + accuracy.operation(num1, num2, operator));
+			}			
 			System.out.print("Again?(Y/N)\n");
 			again = input.next();
 		}
